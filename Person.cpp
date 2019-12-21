6fa852789bd968ff91fd3e89a43f7ed2433d7983
Eilonc285 <50795839+Eilonc285@users.noreply.github.com>
2019-12-21 21:42:38 +0200

    Bug fixes

--- Person.cpp
+++ Person.cpp
@@ -1,10 +1,16 @@
+#pragma once
 #include "Person.h"
 
 
-Person::Person(std::string name1, long id1, int age1): name(name1), id(id1), age(age1)
+Person::Person(std::string name1, long id1, int age1) : name(name1), id(id1), age(age1)
 {
 }
 
-Person::Person(const Person & p): name(p.name), id(p.id), age(p.age)
+Person::Person(const Person& p) : name(p.name), id(p.id), age(p.age)
 {
+}
+
+void Person::Print() const
+{
+	cout << "Name: " << name << " Id: " << id << " Age: " << age << endl;
 }
