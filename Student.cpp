88bea9bb362c202fdbead9154e01a6f3aa4d93f5
RotemLibrati <57578350+RotemLibrati@users.noreply.github.com>
2019-12-22 10:52:46 +0200

    Update Student.cpp
    add func and fix bugs

--- Student.cpp
+++ Student.cpp
@@ -1,10 +1,18 @@
+#pragma once
 #include "Student.h"
+
+Student::Student(int average1, string institude1): average(average1), institute(institude1)
+{
+}
 
 Student::Student(string name1, long id1, int age1, int avg, string inst) :Person(name1, id1, age1), average(avg), institute(inst) {}
 
-void Student::Print() const
+Student::Student(const Student& s): Person(s.name, s.id, s.age), average(s.average), institute(s.institute)
 {
-	Person::print();
-	cout << "The average is : " << average<< endl << "The institute is :" << institute << endl;
 }
 
+void Student::Print() const
+{
+	Person::Print();
+	cout << "The average is : " << average << endl << "The institute is :" << institute << endl;
+}
